f285049546c6adfdb98eea48ddda1cbcba5245ff
Zach Schmidt <zschmidt@uoregon.edu>
2017-04-15 17:37:30 -0700

    Adding notifications to the yml script

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
-# def predictor( row ):
-# 	m_mode = 'Yes'
-# 	h_mode = '1'
-# 	grad = row['Education']  # no empties
-# 	area = row['Property_Area']  # no empties
-# 	married = row['Married'] if row['Married'] != '' else m_mode
-# 	history = row['Credit_History'] if row['Credit_History'] != '' else h_mode
+def predictor( row ):
+	m_mode = 'Yes'
+	h_mode = '1'
+	grad = row['Education']  # no empties
+	area = row['Property_Area']  # no empties
+	married = row['Married'] if row['Married'] != '' else m_mode
+	history = row['Credit_History'] if row['Credit_History'] != '' else h_mode
 		
-# 	if grad == 'Graduate':
-# 		if history == '1':
-# 			return 'Y'
-# 		else:
-# 			return 'N'
-# 	else:
-# 		if area != 'Rural':
-# 			if married == 'Yes':
-# 				return 'Y'
-# 			else:
-# 				return 'N'
-# 		else:
-# 			return 'N'
+	if grad == 'Graduate':
+		if history == '1':
+			return 'Y'
+		else:
+			return 'N'
+	else:
+		if area != 'Rural':
+			if married == 'Yes':
+				return 'Y'
+			else:
+				return 'N'
+		else:
+			return 'N'
 
