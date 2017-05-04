0c9bd5102f546bbe33eb2bac922cb93aa2fa9677
Zach Schmidt <zschmidt@uoregon.edu>
2017-05-03 19:41:35 -0700

    Testing a custom error

--- server/app.js
+++ server/app.js
@@ -45,13 +45,9 @@ app.post('/', function(req, res) {
 
 	var dt = dateTime.create();
 	dt = dt.format('Y-m-d H:M:S');
-
     var testCmd = 'cd '+absolutePath+' && rm submission.py && touch submission.py && echo "'+req.body.code+'">>submission.py && git add . && git commit -m "Auto commit from thoth at '+dt+'" && git push';
-
-
+    console.error("This is a custom error!!");
     console.log("Request ", req);
-
-
     cp.exec(testCmd, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
@@ -63,3 +59,7 @@ app.post('/', function(req, res) {
     res.send('You just POST\'d: ' + JSON.stringify(req.body));
     console.log(req.body);
 })
+
+
+
+
