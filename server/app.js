ef0ee059836886358d3eb6f9e0d67e62740f5cf1
Zach Schmidt <zschmidt@uoregon.edu>
2017-05-21 13:22:57 -0700

    Who cares

--- server/app.js
+++ server/app.js
@@ -21,6 +21,7 @@ app.get('/auth', function(req, res){
     var xhr = new XMLHttpRequest();
     xhr.open('POST', "https://github.com/login/oauth/access_token?client_id=02d1c7baba80ece0140f&redirect_uri=http://thoth.cs.uoregon.edu:3000/&client_secret="+secret+"&code="+code);
     xhr.addEventListener("readystatechange", getAccessCode, false);
+    xhr.send();
 
     function getAccessCode(e) {
         if (xhr.readyState === 4 && xhr.status == 200) {
