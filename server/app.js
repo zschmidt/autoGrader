487ee3459f34e03380694ad9096ef62d9cb96abf
Zach Schmidt <zschmidt@uoregon.edu>
2017-05-21 17:07:19 -0700

    Who cares

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ app.get('/auth', function(req, res){
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
     http.onreadystatechange = function() {//Call a function when the state changes.
+        console.log("Inside -- ", http);
         if(http.readyState == 4 && http.status == 200) {
             console.log(http.responseText);
         }
