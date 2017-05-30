d031c8717bc7a07f5d61d4a7558325b6e925a650
Zach Schmidt <zschmidt@uoregon.edu>
2017-05-30 16:56:38 -0700

    Added code to commit and push to github

--- server/app.js
+++ server/app.js
@@ -77,7 +77,7 @@ app.get('/getSubmission', function(req, res) {
         var lastSubmission = 'curl https://raw.githubusercontent.com/' + login + '/' + module + '/master/submission.py'
         cp.exec(lastSubmission, (error, stdout, stderr) => {
             response.submission = stdout;
-            if(stdout.includes("404: Not Found"))
+            if (stdout.includes("404: Not Found"))
                 response.submission = " ";
             res.send(JSON.stringify(response));
         });
@@ -91,28 +91,99 @@ app.listen(3000, function() {
 
 
 // POST method route
+// This one is a bear.
+// It takes an object that looks like this:
+// {
+//    module: MODULE,
+//    code: SUBMISSION,
+//    access_token: ACCESS_TOKEN,
+//    login: GITHUB_LOGIN
+// }
 app.post('/', function(req, res) {
 
     var dt = dateTime.create();
     dt = dt.format('Y-m-d H:M:S');
 
 
-    //Ready? I'd suggest the documentation to follow along...
+    var module = req.body.module;
+    var repo = module; //Easier to think about this way....
+    var code = req.body.code;
+    var access_token = req.body.access_token;
+    var login = req.body.login;
+
+    //Don't look here. I'm using this as a goto statement... 
+    //  too tired to think of a better way...
+    function repoExists() {
+        // 1.) The repo exists, we need the SHA of the last commit
+        var SHA_LAST_COMMIT = "curl https://api.github.com/repos/" + login + "/" + repo + "/git/refs/heads/master";
+        console.log("SHA_LAST_COMMIT " + SHA_LAST_COMMIT);
+        cp.exec(SHA_LAST_COMMIT, (error, stdout, stderr) => {
+            var response = JSON.parse(stdout);
+            SHA_LAST_COMMIT = response.object.sha; //Oh JavaScript, you dog you.
+            // 2.) We now need the SHA of the base tree
+            var SHA_BASE_TREE = "curl https://api.github.com/repos/" + login + "/" + repo + "/git/commits/" + SHA_LAST_COMMIT;
+            console.log("SHA_BASE_TREE " + SHA_BASE_TREE);
+            cp.exec(SHA_BASE_TREE, (error, stdout, stderr) => {
+                var response = JSON.parse(stdout);
+                SHA_BASE_TREE = response.tree.sha;
+                // 3.) Post out for a new tree -> save the resulting SHA
+                var content = {
+                    "base_tree": SHA_BASE_TREE,
+                    "tree": [{
+                        "path": "submission.py",
+                        "mode": "100644",
+                        "type": "blob",
+                        "content": code
+                    }]
+                };
+                var SHA_NEW_TREE = "curl -H 'Content-Type: application/json' -X POST -d " + JSON.stringify(content) + " https://api.github.com/repos/" + login + "/" + repo + "/git/trees?access_token=" + access_token;
+                console.log("SHA_NEW_TREE " + SHA_NEW_TREE);
+                cp.exec(SHA_NEW_TREE, (error, stdout, stderr) => {
+                    var response = JSON.parse(stdout);
+                    SHA_NEW_TREE = response.sha;
+                    // 4.) Post to get new commit SHA
+                    var content = {
+                        "message": "Auto commit from thoth at " + dt,
+                        "parents": [
+                            SHA_LAST_COMMIT
+                        ],
+                        "tree": SHA_NEW_TREE
+                    }
+                    var SHA_NEW_COMMIT = "curl -H 'Content-Type: application/json' -X POST -d " + JSON.stringify(content) + " https://api.github.com/repos/" + login + "/" + repo + "/git/commits?access_token=" + access_token;
+                    console.log("SHA_NEW_COMMIT "+SHA_NEW_COMMIT);
+                    cp.exec(SHA_NEW_COMMIT, (error, stdout, stderr) => {
+                        var response = JSON.parse(stdout);
+                        SHA_NEW_COMMIT = response.sha;
+                        // 5.) We made it! Push to github!
+                        var push = "curl -H 'Content-Type: application/json' -X POST -d '{\"sha\":" + SHA_NEW_COMMIT + "}' https://api.github.com/repos/" + login + "/" + repo + "/git/refs/heads/master?access_token=" + access_token;
+                        console.log("Push "+push);
+                        cp.exec(push, (error, stdout, stderr) => {
+                            res.send("Successfully pushed to GitHub");
+                        });
+                    });
+                });
+            });
+        });
+    }
 
 
+    // 0.) We've got to check to see if the repo exists
+    var lookForRepo = 'curl https://api.github.com/repos/' + login + '/' + module;
 
+    console.log("lookForRepo " + lookForRepo);
 
-
-
-    var pushCmd = 'cd ' + __dirname + '/../' + ' && rm submission.py && touch submission.py && echo "' + req.body.code + '">>submission.py && git add . && git commit -m "Auto commit from thoth at ' + dt + '" && git push';
-    cp.exec(pushCmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
+    cp.exec(lookForRepo, (error, stdout, stderr) => {
+        var response = JSON.parse(stdout);
+        if (response.message && response.message === "Not Found") {
+            //We haven't created this repo yet
+            console.log("Could not find repo.... creating!");
+            var makeRepo = "curl -H 'Content-Type: application/json' -X POST -d '{\"name\":\"" + module + "\",\"description\":\"This repo holds code submissions for " + module + "\", \"auto_init\":true}' https://api.github.com/user/repos?access_token=" + access_token;
+            console.log("makeRepo " + makeRepo);
+            cp.exec(makeRepo, (error, stdout, stderr) => {
+                repoExists();
+            });
+        } else {
+            repoExists();
         }
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
     });
-    res.send('You just POST\'d: ' + JSON.stringify(req.body));
-    console.log(req.body);
 })
