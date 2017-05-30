bd4803e9fd032937d4ae08c0968efa2911b4d7c2
Zach Schmidt <zschmidt@uoregon.edu>
2017-05-30 15:54:02 -0700

    Who cares

--- server/app.js
+++ server/app.js
@@ -14,28 +14,28 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 // __dirname is /home/zach/autoGrader/server
 
 
-function sendAccessToken(access_token){
-    return "<!doctype html><html lang='en'><head><style type='text/css'>@keyframes spinner{to{transform:rotate(360deg)}}.spinner:before{content:'';box-sizing:border-box;position:absolute;top:50%;left:50%;width:20px;height:20px;margin-top:-10px;margin-left:-10px;border-radius:50%;border-top:2px solid #07d;border-right:2px solid transparent;animation:spinner.6s linear infinite}</style><meta charset='utf-8'><title>Authenticating...</title></head><body><div class='spinner'></div><script type='text/javascript'>function store(){sessionStorage.removeItem('access_token');sessionStorage.setItem('access_token','"+access_token+"');setTimeout(function(){window.location.replace('http://thoth.cs.uoregon.edu:3000')},1000)}window.onload=store;</script></body></html>";
+function sendAccessToken(access_token) {
+    return "<!doctype html><html lang='en'><head><style type='text/css'>@keyframes spinner{to{transform:rotate(360deg)}}.spinner:before{content:'';box-sizing:border-box;position:absolute;top:50%;left:50%;width:20px;height:20px;margin-top:-10px;margin-left:-10px;border-radius:50%;border-top:2px solid #07d;border-right:2px solid transparent;animation:spinner.6s linear infinite}</style><meta charset='utf-8'><title>Authenticating...</title></head><body><div class='spinner'></div><script type='text/javascript'>function store(){sessionStorage.removeItem('access_token');sessionStorage.setItem('access_token','" + access_token + "');setTimeout(function(){window.location.replace('http://thoth.cs.uoregon.edu:3000')},1000)}window.onload=store;</script></body></html>";
 }
 
 //Don't forget to add the client secret to the environment variables!
-app.get('/auth', function(req, res){
+app.get('/auth', function(req, res) {
     var secret = process.env.client_secret;
     var code = req.query.code;
     var url = "https://github.com/login/oauth/access_token";
-    var params = "client_id=02d1c7baba80ece0140f&client_secret="+secret+"&code="+code;
-    
-    if(url && params){ //Sometimes they're undefined.... spooky!
-        console.log('curl --data "'+params+'" '+url);
-        var cmd = 'curl --data "'+params+'" '+url;
+    var params = "client_id=02d1c7baba80ece0140f&client_secret=" + secret + "&code=" + code;
+
+    if (url && params) { //Sometimes they're undefined.... spooky!
+        console.log('curl --data "' + params + '" ' + url);
+        var cmd = 'curl --data "' + params + '" ' + url;
         cp.exec(cmd, (error, stdout, stderr) => {
             access_token = stdout.split("&")[0].split("=")[1];
-            console.log("SUCCESS! Access token: "+access_token);
+            console.log("SUCCESS! Access token: " + access_token);
             res.send(sendAccessToken(access_token));
             res.redirect('/');
         });
     }
-    
+
 
 });
 
@@ -62,16 +62,16 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json())
 
 
-app.get('/getSubmission', function(req, res){
-    var userLogin = 'curl https://api.github.com/user?access_token='+req.query.access_token;
+app.get('/getSubmission', function(req, res) {
+    var module = req.query.module;
+    var userLogin = 'curl https://api.github.com/user?access_token=' + req.query.access_token;
     console.log(userLogin);
     cp.exec(userLogin, (error, stdout, stderr) => {
-        //var result = JSON.stringify(stdout);
-        console.log("Here's the output ", stdout);
-        console.log("Type is ", typeof(stdout));
-        console.log("LOGIN IS ", JSON.parse(stdout).login);
-
-        res.send(stdout.login);
+        var login = JSON.parse(stdout).login;
+        var lastSubmission = 'curl https://raw.githubusercontent.com/' + login + '/' + module + '/master/submission.py'
+        cp.exec(lastSubmission, (error, stdout, stderr) => {
+            res.send(stdout);
+        });
     });
 });
 
@@ -84,9 +84,9 @@ app.listen(3000, function() {
 // POST method route
 app.post('/', function(req, res) {
 
-	var dt = dateTime.create();
-	dt = dt.format('Y-m-d H:M:S');
-    var pushCmd = 'cd '+__dirname+'/../'+' && rm submission.py && touch submission.py && echo "'+req.body.code+'">>submission.py && git add . && git commit -m "Auto commit from thoth at '+dt+'" && git push';
+    var dt = dateTime.create();
+    dt = dt.format('Y-m-d H:M:S');
+    var pushCmd = 'cd ' + __dirname + '/../' + ' && rm submission.py && touch submission.py && echo "' + req.body.code + '">>submission.py && git add . && git commit -m "Auto commit from thoth at ' + dt + '" && git push';
     cp.exec(pushCmd, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
@@ -98,7 +98,3 @@ app.post('/', function(req, res) {
     res.send('You just POST\'d: ' + JSON.stringify(req.body));
     console.log(req.body);
 })
-
-
-
-
